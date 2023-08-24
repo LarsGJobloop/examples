79fef7e7f8b1127d883a669096d69591d038ddc9
LarsG <lars.gunnar@jobloop.no>
2023-08-24 16:10:59 +0000

    setup types for CSS classes to support Tailwind autocomplete

---
+++ birthday-problem/src/types.d.ts
@@ -0,0 +1,7 @@
+// There is probably a better solution to this, but I could not find one
+/**
+ * CSS types for Tailwind Intellisense
+ */
+export type CSSClasses = React.HTMLAttributes<
+  React.ReactHTMLElement<HTMLElement>
+>["className"];
